7eeb7044a08452c9bd360337bd1f41ac86a0db50
Sebastian Cardona <sebasdeveloper89@gmail.com>
2021-10-16 09:41:09 -0500

    Se crea la Arquitectura del proyecto

---
+++ SocialNetwork.Sports.Domain/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Sports.Domain
+{
+    public class User
+    {
+        public int UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public string Email { get; set; }
+    }
+}
