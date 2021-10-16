00c1d43150fe068e9f6d5d242f42da29b6e8fe49
Alexander Gonzalez <alexvasquezghz@gmail.com>
2021-10-16 10:05:47 -0500

    Se crea la clase Comment

---
+++ SocialNetwork.Sports.Domain/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Sports.Domain
+{
+    public class Comment
+    {
+        public string CommentID { get; set; }
+
+        public string CommentDate { get; set; }
+
+        public string Description { get; set; }
+
+
+    }
+}
